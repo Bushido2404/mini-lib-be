dfb4051b2a8294d92ab32e35c823e41b69404fc1
Bushido2404 <57760757+Bushido2404@users.noreply.github.com>
2025-09-26 14:12:00 +0700

    feat: add downloa cover

--- src/modules/book/controllers/book.controller.ts
+++ src/modules/book/controllers/book.controller.ts
@@ -17,6 +17,9 @@ import { CreateBookDto } from '../dto/create-book.dto';
 import { UpdateBookDto } from '../dto/update-book.dto';
 import { bookCoverInterceptor } from '../../../common/interceptor/book-cover.interceptor';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Res } from '@nestjs/common';
+import type { Response } from 'express';
+import { join } from 'path';
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
@@ -50,6 +53,21 @@ export class BookController {
       throw new BadRequestException('Cover is required');
     }
     return this.bookService.updateCover(id, file.filename);
+  }
+
+  @Get(':id/download-cover')
+  async downloadCover(@Param('id') id: string, @Res() res: Response) {
+    const book = await this.bookService.findOne(id);
+    if (!book) {
+      throw new BadRequestException('Book not found');
+    }
+    console.log(book);
+    if (book && !book.cover) {
+      throw new BadRequestException('Book cover not found');
+    }
+    const filePath = join(process.cwd(), 'storages', 'covers', book.cover as string);
+    console.log(filePath);
+    return res.download(filePath, `${book.cover}`);
   }
 
   @Delete(':id')
