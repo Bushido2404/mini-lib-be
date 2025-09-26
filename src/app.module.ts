37fa973cbf31d2376811143cef205f857b795b55
Bushido2404 <57760757+Bushido2404@users.noreply.github.com>
2025-09-26 13:12:08 +0700

    fix: register module into nestjs app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './config/database.module';
 import { UserModule } from './modules/user/user.module';
+import { BookModule } from './modules/book/book.module';
+import { PatronModule } from './modules/patron/patron.module';
+import { LoanModule } from './modules/loan/loan.module';
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, BookModule, PatronModule, LoanModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ValidationError } from 'class-validator';
+import { HttpStatus, HttpException, ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const extractMessages = (errs: ValidationError[], parentPath = ''): string[] => {
+          return errs.flatMap((err) => {
+            // build full path
+            const path = parentPath
+              ? err.property.match(/^\d+$/) // array index?
+                ? `${parentPath}[${err.property}]`
+                : `${parentPath}.${err.property}`
+              : err.property;
+
+            // collect current constraints
+            const constraints = Object.values(err.constraints ?? {});
+            const current = constraints.map((msg) => `${path}: ${msg}`);
+
+            // recurse into children
+            const children = err.children?.length ? extractMessages(err.children, path) : [];
+
+            return [...current, ...children];
+          });
+        };
+
+        const messages = extractMessages(errors);
+
+        return new HttpException(
+          {
+            success: false,
+            statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+            message: messages,
+          },
+          HttpStatus.UNPROCESSABLE_ENTITY
+        );
+      },
+    })
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'storages'), {
+    prefix: '/storages',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
